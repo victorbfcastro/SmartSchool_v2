3b37fdf95da20143d909041379818cbde8023d40
Victor-Acer <victorbfc@hotmail.com>
2021-01-13 22:14:49 -0300

    implement dropdown menu

--- SmartSchoolApp/src/app/app.module.ts
+++ SmartSchoolApp/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { ProfessoresComponent } from './components/professores/professores.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { TituloComponent } from './components/shared/titulo/titulo.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
+    ProfessoresComponent,
+    PerfilComponent,
+    NavComponent,
+    TituloComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
+    ToastrModule.forRoot({
+      timeOut: 3500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
